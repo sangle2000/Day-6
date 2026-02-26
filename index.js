a8957e9801b75eef3ec1b587ebc860eb31ef34eb
sangle2000 <thanhsang22440011@gmail.com>
2026-02-26 20:28:30 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * 1. Hàm là gì? Cách khởi tạo hàm
+ * 2. Tham số, tham trị
+ * 3. Từ khóa return
+ * 4. Function Expression, Arrow Function
+ */
+
+/**
+ * 1. Hàm là một khối câu lệnh, gọi để chạy lại
+ * 2. Cách khởi tạo hàm
+ */
+
+// 1. Decleration Function
+function myFunc(a, b) {
+  console.log(a, b);
+} // Khối câu lệnh - Code Block
+
+myFunc("Hello World", "abc");
+
+/**
+ * 1. Tạo 1 hàm tên là printName
+ * 2. Có 1 tham số tên là name
+ * 3. Trong hàm, in ra lời chào name đó: Hello, Khang
+ */
+
+/**
+ * 1. Tạo 1 hàm tên là introduce
+ * 2. Hàm này sẽ nhận 3 tham số: name, age, address
+ * 3. In ra câu: Xin chào, tôi tên là..., năm nay tôi...tuổi, Tôi đang sinh sống ở...
+ */
